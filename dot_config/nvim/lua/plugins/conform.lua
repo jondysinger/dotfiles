b790f70cb36bbf9138ef033d2e0a1cfc68c0beca
Jon Dysinger <jondysinger@gmail.com>
2026-04-07 14:47:43 -0700

    Added formatters

--- dot_config/nvim/lua/plugins/conform.lua
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -1,26 +1,35 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-      {
-        "<leader>fc",
-        function()
-          require("conform").format({ async = true })
-        end,
-        desc = "Format buffer",
-      },
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>fc",
+      function()
+        require("conform").format({ async = true })
+      end,
+      desc = "Format buffer",
     },
-    opts = {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        nix = { "nixfmt" },
-        rust = { "rustfmt" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-      },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "black" },
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "jq" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      cs = { "csharpier" },
+      nix = { "nixfmt" },
+      rust = { "rustfmt" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
     },
   },
 }
