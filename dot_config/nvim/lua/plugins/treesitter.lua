a764277925cb51220a14beea785e28ae4837492d
Jon Dysinger <jondysinger@gmail.com>
2026-04-08 11:20:04 -0700

    Fixed treesitter autocmd

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,24 +3,31 @@ return {
   commit = "90cd6580e720caedacb91fdd587b747a6e77d61f",
   lazy = false,
   build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "bash",
-      "c_sharp",
-      "css",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "nix",
-      "python",
-      "regex",
-      "rust",
-      "toml",
-      "tsx",
-      "typescript",
-      "yaml",
-    },
-  },
+  config = function()
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        "bash",
+        "c_sharp",
+        "css",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "nix",
+        "python",
+        "regex",
+        "rust",
+        "toml",
+        "tsx",
+        "typescript",
+        "yaml",
+      },
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+  end,
 }
