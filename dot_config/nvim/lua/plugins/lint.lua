97d4da49f0962baed8313f2c725d2fc5c5b6b414
Jon Dysinger <jondysinger@gmail.com>
2026-04-18 22:47:25 -0700

    Hadolint fixes

--- dot_config/nvim/lua/plugins/lint.lua
+++ dot_config/nvim/lua/plugins/lint.lua
@@ -4,6 +4,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       local lint = require("lint")
+      local hadolint = vim.deepcopy(lint.linters.hadolint)
 
       lint.linters_by_ft = {
         python = { "mypy" },
@@ -31,9 +32,42 @@ return {
         end
       end
 
+      local function project_root()
+        local buf_dir = vim.fn.expand("%:p:h")
+        local root_marker = vim.fs.find({ ".hadolint.yaml", ".hadolint.yml", ".git" }, {
+          path = buf_dir,
+          upward = true,
+        })[1]
+
+        if root_marker then
+          return vim.fs.dirname(root_marker)
+        end
+
+        return buf_dir
+      end
+
+      local function hadolint_config()
+        local buf_dir = vim.fn.expand("%:p:h")
+        return vim.fs.find({ ".hadolint.yaml", ".hadolint.yml" }, {
+          path = buf_dir,
+          upward = true,
+        })[1]
+      end
+
+      lint.linters.hadolint = function()
+        local linter = vim.deepcopy(hadolint)
+        local config = hadolint_config()
+
+        if config then
+          linter.args = { "-f", "json", "--config", config, "-" }
+        end
+
+        return linter
+      end
+
       vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave" }, {
         callback = function()
-          local cwd = python_cwd()
+          local cwd = python_cwd() or project_root()
           lint.try_lint(nil, { cwd = cwd })
         end,
       })
