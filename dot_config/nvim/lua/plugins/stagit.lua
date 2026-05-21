82deed172384e059e6a3dbcac680c3de86584d3b
Jon Dysinger <jondysinger@gmail.com>
2026-05-20 21:38:50 -0700

    Changed leader keymap

--- dot_config/nvim/lua/plugins/stagit.lua
+++ dot_config/nvim/lua/plugins/stagit.lua
@@ -3,7 +3,7 @@ return {
     "jondysinger/stagit.nvim",
     cmd = { "StagitToggle", "StagitRefresh", "StagitCommit" },
     keys = {
-      { "<leader>gg", "<cmd>StagitToggle<CR>", desc = "Stagit" },
+      { "<leader>g", "<cmd>StagitToggle<CR>", desc = "Stagit" },
     },
     opts = {
       mappings = {
